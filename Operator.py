2b177627feaf7841db6737e6d303b52d7539b6cc
orienren <kaiibanhhh@gmail.com>
2025-05-04 22:58:01 +0700

    Operatorsksk.py

---
+++ Operator.py
@@ -0,0 +1,27 @@
+a = 10
+b = 3
+
+# Operator Aritmatika
+print("Penjumlahan:", a + b)
+print("Pengurangan:", a - b)
+print("Perkalian:", a * b)
+print("Pembagian:", a / b)
+
+# Operator Perbandingan
+print("Apakah a sama dengan b?", a == b)
+print("Apakah a lebih dari b?", a > b)
+
+# Operator Logika
+x = True
+y = False
+print("x and y:", x and y)
+print("x or y:", x or y)
+
+# Operator Penugasan
+c = 5
+c += 1
+print("c setelah += 1:", c)
+
+# Operator Keanggotaan
+data = [1, 2, 3]
+print("Apakah 2 ada di data?", 2 in data)
