0a77124ff53a6766f98c0a2c6029fa9f4b102ea3
orienren <kaiibanhhh@gmail.com>
2025-05-04 22:39:00 +0700

    komentarcinta.py

---
+++ komentar.py
@@ -0,0 +1,10 @@
+# Ini adalah komentar satu baris
+
+"""
+Ini komentar
+lebih dari satu baris
+biasanya dipakai untuk dokumentasi
+"""
+
+# Komentar digunakan untuk menjelaskan kode agar mudah dipahami orang lain
+nama = "Cinta"  # Variabel nama berisi string "Cinta"
