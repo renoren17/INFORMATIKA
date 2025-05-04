746294fe9eafd4a30079a1a4f3185d66d4707d7a
orienren <kaiibanhhh@gmail.com>
2025-05-04 22:52:53 +0700

    TipeDatasksk.py

---
+++ TipeData.py
@@ -0,0 +1,38 @@
+#tipeData
+
+# Tipe Data Boolean
+print("Tipe Boolean:", True)
+
+# Tipe Data String
+print("Tipe String 1:", 'You know, you know where you are with')
+
+# Tipe Data Integer
+print("Tipe Integer:", -20)
+
+# Tipe Data Float
+print("Tipe Float:", 3.14)
+
+# Tipe Data Hexadecimal
+print("Tipe Hexadecimal:", hex(23))  # menghasilkan '0x17'
+
+# Tipe Data Complex
+print("Tipe Complex:", 5j)
+
+# Tipe Data List
+print("Tipe List Angka:", [1, 2, 3, 4, 5])
+print("Tipe List String:", ["satu", "dua", "tiga"])
+
+# Tipe Data Tuple
+print("Tipe Tuple Angka:", (1, 2, 3, 4, 5))
+print("Tipe Tuple String:", ("satu", "dua", "tiga"))
+
+# Tipe Data Dictionary (langsung cetak)
+print("Tipe Dictionary:", {"nama": "Cinta", "umur": 16})
+
+# Tipe Data Dictionary (dalam variabel)
+biodata = {"nama": "Cinta Nayla Janice", "umur": 16}
+print("Variabel biodata:", biodata)
+print("Tipe data biodata:", type(biodata))
+
+# Bonus: Gabung string
+print("Nama saya: " + "Cinta")
