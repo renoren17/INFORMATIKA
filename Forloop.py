c5812291a36ea33e95f1c1664b3239f5d74b413f
orienren <kaiibanhhh@gmail.com>
2025-05-08 13:46:18 +0700

    Forloop.py
    Forloop.sksk

---
+++ Forloop.py
@@ -0,0 +1,11 @@
+#For loop
+list_angka = [1, 2, 3, 4, 5]
+for angka in list_angka:
+    print(angka)
+
+makanan_favorit = ["keju", "mi ayam"]
+for makanan in makanan_favorit:
+    print("Saya suka makan", makanan)
+
+for angka in range(1, 10):
+    print(angka)
